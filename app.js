6045ecb678fdc0c8b909eaad190866ce101d5b59
Ahmet Eren Sancar <aes.sncr@gmail.com>
2021-05-28 21:01:49 +0300

    done

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const ejs = require("ejs");
 
 const app = express();
 
+// TEMPLATE ENGINES
+app.set("view engine", "ejs");
 
 //MIDDLEWARES
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/add", (req, res) => {
+  res.render("add");
+});
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Sunucu ${port} portunda başlatıldı..`);
+  console.log(`Server started on port ${port}...`);
 });

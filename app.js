8be1732ec3a339ad5adcc2e225ea7ac80da01715
Ahmet Eren Sancar <aes.sncr@gmail.com>
2021-05-29 23:50:23 +0300

    ...

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
 const ejs = require("ejs");
+const Photo = require('./models/Photo');
 
 const app = express();
+
+//connect database
+mongoose.connect("mongodb://localhost/pcat-test-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // TEMPLATE ENGINES
 app.set("view engine", "ejs");
 
 //MIDDLEWARES
-
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({})
+  res.render('index', { photos
+   });
 });
 
-app.get("/about", (req, res) => {
-  res.render("about");
+app.get('/about', (req, res) => {
+  res.render('about');
 });
 
-app.get("/add", (req, res) => {
-  res.render("add");
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body)            
+  res.redirect('/')
 });
 
 const port = 3000;

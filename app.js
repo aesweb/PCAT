8a3fd33f1ffc1eb6391931ab8790f2689fdaa82f
Ahmet Eren Sancar <aes.sncr@gmail.com>
2021-05-31 23:06:49 +0300

    update photos

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const ejs = require("ejs");
-const Photo = require('./models/Photo');
+const Photo = require("./models/Photo");
+const fileUpload = require("express-fileupload");
+const methodOverride = require('method-override');
+
+
 
 const app = express();
 
@@ -19,32 +23,65 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
+app.use(methodOverride('_method'));
 
-app.get('/', async (req, res) => {
-  const photos = await Photo.find({})
-  res.render('index', { 
-    photos
-   });
+app.get("/", async (req, res) => {
+  const photos = await Photo.find({});
+  res.render("index", {
+    photos,
+  });
 });
 
-app.get('/photos/:id', async (req, res) => {
-  const photo = await Photo.findById(req.params.id) 
-  res.render('photo', {
-    photo
-  })
+app.get("/photos/:id", async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  res.render("photo", {
+    photo,
+  });
 });
 
-app.get('/about', (req, res) => {
-  res.render('about');
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
-app.get('/add', (req, res) => {
-  res.render('add');
+app.get("/add", (req, res) => {
+  res.render("add");
 });
 
-app.post('/photos', async (req, res) => {
-  await Photo.create(req.body)            
-  res.redirect('/')
+app.post("/photos", async (req, res) => {
+    const uploadDir = "public/uploads";
+    const fs = require('fs');
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadeImage = req.files.image;
+  let uploadPath = __dirname + "/public/uploads/" + uploadeImage.name;
+
+  uploadeImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadeImage.name,
+    });
+    res.redirect('/');
+  });
+});
+
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  res.render('edit', {
+    photo,
+  });
+});
+
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title
+  photo.description = req.body.description
+  photo.save()
+
+  res.redirect(`/photos/${req.params.id}`)
 });
 
 const port = 3000;
